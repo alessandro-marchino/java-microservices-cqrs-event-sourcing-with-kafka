d60b1778a855cefa073b40cf99db6c1ba9cfeb4b
amarchino <marchino.alessandro@gmail.com>
2024-05-18 17:39:41 +0200

    Lesson 26

---
+++ cqrs.core/src/main/java/com/techbank/cqrs/core/events/EventModel.java
@@ -0,0 +1,23 @@
+package com.techbank.cqrs.core.events;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document(collation = "eventStore")
+public class EventModel {
+	@Id
+	private String id;
+	private OffsetDateTime timestamp;
+	private String aggregateIdentifier;
+	private String aggregateType;
+	private int version;
+	private String eventType;
+	private BaseEvent eventData;
+}
