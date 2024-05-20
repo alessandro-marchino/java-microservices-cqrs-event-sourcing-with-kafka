5b0e5c69da2330eb5ec0d821b131b13f9e02d4df
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-20 13:03:28 +0200

    Lesson 29

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventStore.java
@@ -0,0 +1,57 @@
+package com.techbank.account.cmd.infrastructure;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.account.cmd.domain.EventStoreRepository;
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.events.EventModel;
+import com.techbank.cqrs.core.exceptions.AggregateNotFoundException;
+import com.techbank.cqrs.core.exceptions.ConcurrencyException;
+import com.techbank.cqrs.core.infrastructure.EventStore;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountEventStore implements EventStore {
+	private final EventStoreRepository eventStoreRepository;
+
+	@Override
+	public void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
+		var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+		if(expectedVersion != -1 && eventStream.getLast().getVersion() != expectedVersion) {
+			throw new ConcurrencyException();
+		}
+		var version = expectedVersion;
+		for(var event : events) {
+			version++;
+			event.setVersion(version);
+			var eventModel = EventModel.builder()
+					.timestamp(OffsetDateTime.now())
+					.aggregateIdentifier(aggregateId)
+					.aggregateType(AccountAggregate.class.getTypeName())
+					.version(version)
+					.eventType(event.getClass().getTypeName())
+					.eventData(event)
+					.build();
+			var persistedEvent = eventStoreRepository.save(eventModel);
+			if(persistedEvent != null) {
+				// TODO: produce event in Kafka
+			}
+		}
+	}
+
+	@Override
+	public List<BaseEvent> getEvents(String aggregateId) {
+		var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+		if(eventStream == null || eventStream.isEmpty()) {
+			throw new AggregateNotFoundException("Incorrect aggregate ID provided");
+		}
+		return eventStream.stream().map(x -> x.getEventData()).toList();
+	}
+
+}
---
+++ cqrs.core/src/main/java/com/techbank/cqrs/core/exceptions/ConcurrencyException.java
@@ -0,0 +1,7 @@
+package com.techbank.cqrs.core.exceptions;
+
+public class ConcurrencyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
