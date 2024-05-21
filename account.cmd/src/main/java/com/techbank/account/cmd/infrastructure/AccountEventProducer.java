a4e50b3f25d5114c30ba110f974b84a1721b6cb1
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-21 14:13:05 +0200

    Lesson 37

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventProducer.java
@@ -0,0 +1,21 @@
+package com.techbank.account.cmd.infrastructure;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.producers.EventProducer;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountEventProducer implements EventProducer {
+	private final KafkaTemplate<String, Object> kafkaTemplate;
+
+	@Override
+	public void produce(String topicName, BaseEvent event) {
+		this.kafkaTemplate.send(topicName, event);
+	}
+
+}
