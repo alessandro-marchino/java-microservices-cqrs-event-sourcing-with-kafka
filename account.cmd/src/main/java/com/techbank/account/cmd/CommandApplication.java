96b56aff78b94b82f1fe4fdcb14421467c64665e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-21 12:43:16 +0200

    Lesson 35

--- account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,13 +1,35 @@
 package com.techbank.account.cmd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.techbank.account.cmd.api.commands.CloseAccountCommand;
+import com.techbank.account.cmd.api.commands.CommandHandler;
+import com.techbank.account.cmd.api.commands.DepositFundsCommand;
+import com.techbank.account.cmd.api.commands.OpenAccountCommand;
+import com.techbank.account.cmd.api.commands.WithdrawFundsCommand;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class CommandApplication {
+
+	private final CommandDispatcher commandDispatcher;
+	private final CommandHandler commandHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
 }
