84c9eb488024e9ca5e32d418ae0b46b9682ff91a
amarchino <marchino.alessandro@gmail.com>
2024-05-25 20:58:42 +0200

    Lesson 47

---
+++ account.query/src/main/java/com/techbank/account/query/infrastructure/consumers/AccountEventConsumer.java
@@ -0,0 +1,48 @@
+package com.techbank.account.query.infrastructure.consumers;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+import com.techbank.account.query.infrastructure.handlers.EventHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountEventConsumer implements EventConsumer {
+	private final EventHandler eventHandler;
+
+	@Override
+	@KafkaListener(topics = "AccountsOpenedEvent", groupId = "${spring.kafka.consumer.groupId")
+	public void consume(AccountOpenedEvent event, Acknowledgment ack) {
+		eventHandler.on(event);
+		ack.acknowledge();
+	}
+
+	@Override
+	@KafkaListener(topics = "FundsDepositedEvent", groupId = "${spring.kafka.consumer.groupId")
+	public void consume(FundsDepositedEvent event, Acknowledgment ack) {
+		eventHandler.on(event);
+		ack.acknowledge();
+	}
+
+	@Override
+	@KafkaListener(topics = "FundsWithdrawnEvent", groupId = "${spring.kafka.consumer.groupId")
+	public void consume(FundsWithdrawnEvent event, Acknowledgment ack) {
+		eventHandler.on(event);
+		ack.acknowledge();
+	}
+
+	@Override
+	@KafkaListener(topics = "AccountClosedEvent", groupId = "${spring.kafka.consumer.groupId")
+	public void consume(AccountClosedEvent event, Acknowledgment ack) {
+		eventHandler.on(event);
+		ack.acknowledge();
+	}
+
+}
