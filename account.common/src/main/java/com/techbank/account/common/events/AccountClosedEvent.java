7421ac9c339c6bee74ad80166d85fd6a2b7a25ac
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-27 12:45:36 +0200

    Lesson 57

--- account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
+++ account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -4,10 +4,12 @@ import com.techbank.cqrs.core.events.BaseEvent;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @SuperBuilder
 public class AccountClosedEvent extends BaseEvent {
 
