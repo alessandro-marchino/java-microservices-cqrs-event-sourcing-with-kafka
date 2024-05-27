8a828e8072091f091ad8460711884c1c84e51169
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-27 12:29:37 +0200

    Lesson 54

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,43 @@
+package com.techbank.account.cmd.api.controllers;
+
+import java.text.MessageFormat;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techbank.account.cmd.api.commands.DepositFundsCommand;
+import com.techbank.account.common.dto.BaseResponse;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/withdrawFunds")
+@Slf4j
+public class WithdrawFundsController {
+
+	private final CommandDispatcher commandDispatcher;
+
+	@PutMapping("/{id}")
+	public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable("id") String id, @RequestBody DepositFundsCommand command) {
+		command.setId(id);
+		try {
+			commandDispatcher.send(command);
+			return new ResponseEntity<>(new BaseResponse("Withdraw funds request completed successfully"), HttpStatus.OK);
+		} catch (IllegalStateException e) {
+			log.warn("Client made a bad request - {}.", e.toString());
+			return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			var safeErrorMessage = MessageFormat.format("Error while processing request to withdraw funds to bank account with id - {0}.", id);
+			log.error("{}", safeErrorMessage, e);
+			return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
