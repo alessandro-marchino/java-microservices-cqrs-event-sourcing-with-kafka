661e229648ba2397a9fa3cb04ce55fbb6f7fd197
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-27 12:40:50 +0200

    Lesson 56

--- account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/CloseAccountController.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/CloseAccountController.java
@@ -4,13 +4,12 @@ import java.text.MessageFormat;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.techbank.account.cmd.api.commands.WithdrawFundsCommand;
+import com.techbank.account.cmd.api.commands.CloseAccountCommand;
 import com.techbank.account.common.dto.BaseResponse;
 import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
 
@@ -19,23 +18,22 @@ import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/withdrawFunds")
+@RequestMapping("/api/v1/closeAccount")
 @Slf4j
-public class WithdrawFundsController {
+public class CloseAccountController {
 
 	private final CommandDispatcher commandDispatcher;
 
-	@PutMapping("/{id}")
-	public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable("id") String id, @RequestBody WithdrawFundsCommand command) {
-		command.setId(id);
-		try {
-			commandDispatcher.send(command);
-			return new ResponseEntity<>(new BaseResponse("Withdraw funds request completed successfully"), HttpStatus.OK);
+	@DeleteMapping("/{id}")
+	public ResponseEntity<BaseResponse> closeAccount(@PathVariable("id") String id) {
+			try {
+			commandDispatcher.send(new CloseAccountCommand(id));
+			return new ResponseEntity<>(new BaseResponse("Bank account closure request completed successfully"), HttpStatus.OK);
 		} catch (IllegalStateException e) {
 			log.warn("Client made a bad request - {}.", e.toString());
 			return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
 		} catch (Exception e) {
-			var safeErrorMessage = MessageFormat.format("Error while processing request to withdraw funds to bank account with id - {0}.", id);
+			var safeErrorMessage = MessageFormat.format("Error while processing request to close bank account with id - {0}.", id);
 			log.error("{}", safeErrorMessage, e);
 			return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
