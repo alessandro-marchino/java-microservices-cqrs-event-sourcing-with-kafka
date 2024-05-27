1e4f73923437a982b020fd5c3612fde8fa34be47
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-27 14:14:46 +0200

    Lesson 65

--- account.query/src/main/java/com/techbank/account/query/QueryApplication.java
+++ account.query/src/main/java/com/techbank/account/query/QueryApplication.java
@@ -3,11 +3,32 @@ package com.techbank.account.query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.techbank.account.query.api.query.FindAccountByHolderQuery;
+import com.techbank.account.query.api.query.FindAccountByIdQuery;
+import com.techbank.account.query.api.query.FindAccountsWithBalanceQuery;
+import com.techbank.account.query.api.query.FindAllAccountsQuery;
+import com.techbank.account.query.api.query.QueryHandler;
+import com.techbank.cqrs.core.infrastructure.QueryDispatcher;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class QueryApplication {
+	private final QueryDispatcher queryDispatcher;
+	private final QueryHandler queryHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
+	}
+
+	@PostConstruct
+	public void registerHandlers() {
+		queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByHolderQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountsWithBalanceQuery.class, queryHandler::handle);
 	}
 
 }
